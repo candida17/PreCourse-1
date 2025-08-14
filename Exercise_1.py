de09d7c306e3df31fd3d1760141a34392bb6c5fe
candida17 <53035166+candida17@users.noreply.github.com>
2025-08-15 04:33:37 +0530

    Update Exercise_1.py
    Implement stack using arrays

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,44 @@
+#Time Complexity: isEmpty, push, pop, peek, size - O(1) and show - O(n)
+#Space Complexity: O(n)
+#n refers to number of elements in stack
+
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+       self.items = []
          
      def isEmpty(self):
+       #Check if stack is empty
+       return len(self.items) == 0
          
      def push(self, item):
+       #Add an item to the top of the stack
+       self.items.append(item)
          
      def pop(self):
+       #Remove and return the top item 
+       if self.isEmpty():
+         return None
+       return self.items.pop()
         
         
      def peek(self):
+       #Return the top item in stack
+       if self.isEmpty():
+         return None
+        return self.items[-1]
         
      def size(self):
+       #Return the total items in the stack
+       return len(self.items)
          
      def show(self):
+       #Display the stack elements
+       if self.isEmpty():
+         print("Stack is empty")
+       else:
+         print("Final Stack: ", self.items[::-1])
          
 
 s = myStack()
