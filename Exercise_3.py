a4d8f57967d25ffe7c89118559a98dcb5676cfa0
candida17 <53035166+candida17@users.noreply.github.com>
2025-08-15 05:18:39 +0530

    Update Exercise_3.py
    Implement Singly linked list

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,8 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = None
     
 class SinglyLinkedList:
     def __init__(self):
@@ -17,6 +19,14 @@ class SinglyLinkedList:
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        new_node = ListNode(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
         
     def find(self, key):
         """
@@ -24,9 +34,40 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current = self.head
+        while current:
+            if current.data == key:
+                return current   #return element
+            current = current.next
+        return None #key not found
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        if current and current.data == key:    #key is present in head of the linked list
+            self.head = current.next
+            return
+        prev = None
+        while current and current.data != key:
+            prev = current
+            current = current.next
+        #key is not found
+        if current is None:
+            return
+        #Remove the node
+        prev.next = current.next
+
+linkedList = SinglyLinkedList()
+linkedList.append(10)
+linkedList.append(20)
+linkedList.append(30)
+linkedList.append(20)
+
+linkedList.remove(20) #removes the first occurred 20 => [10,30,20]
+print(linkedList.find(20))
+print(linkedList.find(40))
+
+
